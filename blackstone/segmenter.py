3c6fddba58df0013a4cbc82784f69950c9b8e403
ICLR&D <48757747+ICLRandD@users.noreply.github.com>
2019-09-24 23:09:35 +0100

    Create segmenter.py

---
+++ blackstone/segmenter.py
@@ -0,0 +1,66 @@
+from typing import List, Any
+from spacy.tokens import Doc, Token
+
+
+def sentence_segmenter(doc: Doc) -> Doc:
+    """Adds custom sentence boundaries to spaCy Doc. 
+
+    @param doc: the spaCy document to be annotated with sentence boundaries
+    """
+    for i, token in enumerate(doc[:-2]):
+        if token.text == "Sch" and doc[i+1].is_punct:
+            doc[i+2].is_sent_start = False
+        if str(doc[i]) == "]" and doc[i+1].is_digit:
+            doc[i+2].is_sent_start = False
+        if str(doc[i+1]) == ")":
+            doc[i+2].is_sent_start = False
+        if str(doc[i+1]) == "(":
+            doc[i+2].is_sent_start = False
+        if str(doc[i+1]) == "]":
+            doc[i+2].is_sent_start = False
+        if str(doc[i+1]) == "[":
+            doc[i+2].is_sent_start = False
+        if doc[i+1].is_digit:
+            doc[i+2].is_sent_start = False
+        if str(doc[i+1]) == ";":
+            doc[i+2].is_sent_start = False
+        if str(doc[i+1]) == "-":
+            doc[i+2].is_sent_start = False
+        if str(doc[i+2]) == "[":
+            doc[i+2].is_sent_start = False
+        if doc[i].is_digit and str(doc[i+1]) == "]":
+            doc[i+1].is_sent_start = False
+        if doc[i].is_digit:
+            doc[i+1].is_sent_start = False
+        if token.text == "?" and doc[i+1].is_quote:
+            doc[i+1].is_sent_start = False
+        if token.text == "ex":
+            doc[i].is_sent_start = False
+        if token.text == "ex" and str(doc[i+1]) == "parte":
+            doc[i+2].is_sent_start = False
+        if token.text == "cf" and str(doc[i+1]) == ".":
+            doc[i+2].is_sent_start = False
+        if token.text == "unreported" and str(doc[i+1]) == ":":
+            doc[i+2].is_sent_start = False
+        if token.text == "Hon" and str(doc[i+1]) == ".":
+            doc[i+2].is_sent_start = False
+        if token.text == "." and doc[i+1].is_quote:
+            doc[i+1].is_sent_start = False
+        if token.text == "?" and str(doc[i+1]) == "!":
+            doc[i+1].is_sent_start = False
+        if token.text == "!" and str(doc[i+1]) == "?":
+            doc[i+1].is_sent_start = False
+        if str(doc[i+2]) == "(":
+            doc[i+2].is_sent_start = False
+        if token.text == "reg" and doc[i+1].is_punct:
+            doc[i+2].is_sent_start = False
+        if token.text == "’s":
+            doc[i].is_sent_start = False
+        if doc[i].is_quote:
+            doc[i+1].is_sent_start = False
+        if token.text == ")" and doc[i+1].is_punct == False:
+            doc[i+2].is_sent_start = False
+        if token.text == "Eu" and doc[i+1].is_punct:
+            doc[i+2].is_sent_start = False
+
+    return doc
