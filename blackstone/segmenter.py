d7c66fe474904d2fcb7a19d3d7364cb4da517144
ICLR&D <48757747+ICLRandD@users.noreply.github.com>
2019-10-07 15:23:30 +0100

    add new rules for breaks following colon and newlines

--- blackstone/segmenter.py
+++ blackstone/segmenter.py
@@ -2,9 +2,8 @@ from typing import List, Any
 from spacy.tokens import Doc, Token
 
 
-def sentence_segmenter(doc: Doc) -> Doc:
+def sentence_segmenter(doc):
     """Adds custom sentence boundaries to spaCy Doc. 
-
     @param doc: the spaCy document to be annotated with sentence boundaries
     """
     for i, token in enumerate(doc[:-2]):
@@ -24,6 +23,12 @@ def sentence_segmenter(doc: Doc) -> Doc:
             doc[i+2].is_sent_start = False
         if str(doc[i+1]) == ";":
             doc[i+2].is_sent_start = False
+        if str(doc[i+1]) == ":":
+            doc[i+2].is_sent_start = True
+        if str(doc[i+1]) == "\n\n":
+            doc[i+2].is_sent_start = True
+        if str(doc[i+1]) == "\n":
+            doc[i+2].is_sent_start = True
         if str(doc[i+1]) == "-":
             doc[i+2].is_sent_start = False
         if str(doc[i+2]) == "[":
@@ -38,6 +43,8 @@ def sentence_segmenter(doc: Doc) -> Doc:
             doc[i].is_sent_start = False
         if token.text == "ex" and str(doc[i+1]) == "parte":
             doc[i+2].is_sent_start = False
+        if token.text == "\n" and str(doc[i+1]) == "\n":
+            doc[i+2].is_sent_start = True
         if token.text == "cf" and str(doc[i+1]) == ".":
             doc[i+2].is_sent_start = False
         if token.text == "unreported" and str(doc[i+1]) == ":":
